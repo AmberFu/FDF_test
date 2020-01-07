e3a5a7d9a630446f78479da4d2bca9bf51724024
Po-ying Fu <pfu@e1z2r3p13.42.us.org>
2020-01-06 21:25:26 -0800

    no leak and new struct

--- src/fdf.h
+++ src/fdf.h
@@ -6,7 +6,7 @@
 /*   By: pfu <spashleyfu@gmail.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 19:32:39 by pfu               #+#    #+#             */
-/*   Updated: 2019/12/14 20:59:31 by pfu              ###   ########.fr       */
+/*   Updated: 2019/12/17 21:12:32 by pfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,43 @@
 typedef struct		s_grid
 {
 	int				row;
-	char			*arr; // may change to a struct: row_len(Y) and int array[]
+	char			*arr; //number string
 	struct s_grid	*next;
 }					t_grid;
 
-typedef struct		s_info
+typedef	struct		s_3d_point
 {
 	int				x;
 	int				y;
+	int				z;
+}					t_3d_point;
+
+// x' = x / (z + 1)
+// y' = y / (z + 1)
+typedef struct 		s_new_2d
+{
+	double			x;
+	double			y;
+};
+
+typedef struct		s_info
+{
+	int				rows;
+	int				cols;
+	int				max_z;
+	int				min_z;
+	int				win_h;
+	int				win_w;
+	t_3d_point			**ori_xyz_3d; // 2D array of t_point
 }					t_info;
 
-int		read_draw_fdf(int fd);
+//int					read_draw_fdf(int fd, t_info **info);
+int					read_draw_fdf(int fd);
+t_grid				*get_grid(char *line, int row);
+void				print_grid_array(t_grid *grid_head);
+void				free_grid(t_grid **grid_head);
 
-t_grid	*get_grid(char *line, int row);
-void	print_grid_array(t_grid **grid_head);
-void	free_grid(t_grid **grid_head);
+t_info  *create_info_struct(void);
+void    print_info(t_info *info);
 
 #endif
--- src/read_draw.c
+++ src/read_draw.c
@@ -6,7 +6,7 @@
 /*   By: pfu <spashleyfu@gmail.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 20:04:15 by pfu               #+#    #+#             */
-/*   Updated: 2019/12/14 21:03:40 by pfu              ###   ########.fr       */
+/*   Updated: 2020/01/06 12:16:54 by pfu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,65 +24,163 @@ t_grid	*get_grid(char *line, int row)
 	return (grid);
 }
 
-void	print_grid_array(t_grid **grid_head)
+/************* For test: *****************/
+void	print_grid_array(t_grid *grid_head)
 {
-	t_grid	*ptr;
 	int		i;
 
+	/****/
+	ft_putstr("\n\nprint_grid_array():\n");
+	/****/
 	i = 0;
-	ptr = *grid_head;
-	while (ptr != NULL)
+	while (grid_head != NULL)
 	{
 		ft_putstr("row[");
-		ft_putnbr(i);
+		ft_putnbr(grid_head->row);
 		ft_putstr("] ");
 		ft_putstr("ptr->arr = \n");
-		ft_putstr(ptr->arr);
+		ft_putstr(grid_head->arr);
 		ft_putchar('\n');
-		ptr = ptr->next;
+		grid_head = grid_head->next;
 		i++;
 	}
 }
+/********************************************/
 
 void	free_grid(t_grid **grid_head)
 {
-	t_grid  *ptr;
+	t_grid	*ptr;
+	t_grid	*next;
 
 	ptr = *grid_head;
 	while (ptr != NULL)
 	{
 		free(ptr->arr);
-		ptr = ptr->next;
+		next = ptr->next;
+		free(ptr);
+		ptr = next;
 	}
 	free(grid_head);
 }
 
-int		read_draw_fdf(int fd)
+t_info	*create_info_struct(void)
 {
-	int		x;
-	char	*line;
-	t_grid	*grid_ptr;
-	t_grid	**grid_head;
+	t_info	*info;
 
-	x = 0;
-	if ((grid_head = (t_grid **)malloc(sizeof(t_grid *))) == NULL)
-		return (-1);
+	if ((info = (t_info *)malloc(sizeof(t_info))) == NULL)
+        return (NULL);
+	info->rows = 0;
+	info->cols = 0;
+	info->max_z = 0;
+	info->min_z = 0;
+	info->ori_xyz_3d = NULL;
+	return (info);
+}
+
+void	free_info(t_info *info)
+{
+	/*
+	int		i;
+
+	i = -1;
+	while (++i < info->y) // row
+		free(info->matrix[i]);
+	free(matrix);*/
+	free(info);
+}
+
+/********************************************/
+
+void	print_info(t_info *info)
+{
+	ft_putstr("\nPRINT INFO:\n");
+	ft_putstr("info->x = ");
+	ft_putnbr(info->rows);
+	ft_putchar('\n');
+	ft_putstr("info->y = ");
+	ft_putnbr(info->cols);
+	ft_putchar('\n');
+	ft_putstr("info->max_z = ");
+	ft_putnbr(info->max_z);
+	ft_putchar('\n');
+	ft_putstr("info->min_z = ");
+	ft_putnbr(info->min_z);
+	ft_putstr("\n\n");
+	// free_info(info);
+}
+
+/********************************************/
+
+void	file_to_grid(int fd, t_grid	**grid_head)
+{
+	t_grid	*grid_ptr;
+	int		rows_nbr;
+	char	*line;
+
+	rows_nbr = 0;
 	while (get_next_line(fd, &line) > 0)
 	{
-		if (x == 0) //First list
+		if (rows_nbr == 0) //First list
 		{
-			grid_ptr = get_grid(line, x);
-			grid_head = &grid_ptr;
+			grid_ptr = get_grid(line, rows_nbr);
+			*grid_head = grid_ptr;
 		}
 		else //others
 		{
-			grid_ptr->next = get_grid(line, x);
+			grid_ptr->next = get_grid(line, rows_nbr);
 			grid_ptr = grid_ptr->next;
 		}
 		free(line);
-		x++;
+		rows_nbr++;
 	}
-	print_grid_array(grid_head);
-	//free_grid(grid_head);
+}
+
+int		get_line_info(char *arr, t_info	**info_struct)
+{
+	/* Get info */
+}
+
+int		grid_to_info(t_grid	*grid_head, t_info	**info_struct)
+{
+	int		last_row;
+	int		col_size;
+	int		first_col_size;
+
+// Get 1) How many line? (x pos)...loop until last one + 1 (start from 0)
+//     2) How many columns? (y pos)...col size must be the same number
+	first_col_size = get_line_size(grid_head->arr);
+	while (grid_head != NULL)
+	{
+		last_row = grid_head->row;
+		// col_size = get_line_info(grid_head->arr);
+		col_size = get_line_info(grid_head->arr, info_struct);
+		if (col_size != first_col_size)
+			return (-1);
+		grid_head = grid_head->next;
+	}
+
+
+	*info_struct = create_info_struct();
+	print_info(*info_struct);
+}
+
+
+/* Do validate!!! -> directly put into int 2d ? */
+int		read_draw_fdf(int fd)
+{
+	t_grid	**grid_head;
+	t_info	*info_struct;
+
+	if ((grid_head = (t_grid **)malloc(sizeof(t_grid *))) == NULL)
+		return (-1);
+	file_to_grid(fd, grid_head);
+	print_grid_array(*grid_head);
+	// save t_grid to 2D interger array:
+	if (grid_to_info(*grid_head, &info_struct) < 0)
+		return (-1);
+
+	// FREE memory:
+	free_info(info_struct);
+	free_grid(grid_head);
 	return (0);
 }
